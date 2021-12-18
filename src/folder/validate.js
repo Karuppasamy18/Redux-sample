2683dd6c032a3e02bc3533341ac67faf4855fe38
Angapparaja <rajakaruppu18@gmail.com>
2021-12-18 04:04:52 -0500

    second

--- src/folder/validate.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-
- export const validateform = yup.object().shape({
-  
-    name :yup.string().required(),
-    password : yup.string().min(3).max(8).required(),
-    email:yup.string().email().required(),
-    
-})
